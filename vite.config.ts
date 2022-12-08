d4a9d4583ffe6113d351cf470a2ff45381e9033e
linchuanke <310504719@qq.com>
2022-12-08 16:08:55 +0800

    add vitest.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/cypress/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress}.config.*",
+      "**/components/Counter.spec.tsx",
+    ],
+  },
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
@@ -13,4 +25,4 @@ export default defineConfig({
     // 省略后缀名
     extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
   },
-})
+});
